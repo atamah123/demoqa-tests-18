11bd455cec84be6d03e15f52852e55dfc35bb8f0
Sergey Atamanchuk <atamah123@gmail.com>
2024-11-13 16:06:37 +0300

    Добавлен JUnitExamples.java

--- src/test/java/JUnitExamples.java
+++ src/test/java/JUnitExamples.java
@@ -1,2 +1,34 @@
+import org.junit.jupiter.api.*;
+
 public class JUnitExamples {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Eto beforeAll");
+    }
+
+    @BeforeEach
+     void beforeEach() {
+        System.out.println("    Eto beforeEach");
+    }
+
+    @AfterAll
+    static void AfterAll() {
+        System.out.println("Eto AfterAll");
+    }
+
+    @AfterEach
+    void AfterEach() {
+        System.out.println("    Eto AfterEach");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("          Eto pervii test");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("          Eto vtoroi test");
+    }
 }
