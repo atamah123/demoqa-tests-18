1f91a60ec18419bb6bada484db3bbca80edae97d
Sergey Atamanchuk <atamah123@gmail.com>
2024-11-13 17:54:33 +0300

    test

--- src/test/java/TextBox.java
+++ src/test/java/TextBox.java
@@ -17,6 +17,8 @@ public class TextBox {
 
     @Test
     void fillFormTest() {
+
+        
             open("/text-box");
             $("[id =userName]").setValue("Alex Egorov");
 //        $("#userName").setValue("Alex Egorov"); // аналогичный вариант
