637132861c7f3b10c0899aeb7c5c1f000ed6aa48
Sergey Atamanchuk <atamah123@gmail.com>
2024-11-13 18:28:03 +0300

    Обновлен CssXpath

--- src/test/java/CssXpathExamples.java
+++ src/test/java/CssXpathExamples.java
@@ -1,2 +1,9 @@
+import static com.codeborne.selenide.Selectors.by;
+import static com.codeborne.selenide.Selenide.$;
+
 public class CssXpathExamples {
+    void cssXpathExamples() {
+        $("[data-testidemail]").setValue("1");
+        // $(by("data-testid", "email")).setValue("1"); // альтернативный вариант
+    }
 }
