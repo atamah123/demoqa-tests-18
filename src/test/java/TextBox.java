381b8f8c47cf4b337fb5462f5d85625d594d5f4d
Sergey Atamanchuk <atamah123@gmail.com>
2024-11-13 17:49:42 +0300

    Добавлен тест в TextBox.java, в build.gradle добавлена зависимость

--- src/test/java/TextBox.java
+++ src/test/java/TextBox.java
@@ -1,10 +1,37 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class TextBox {
 
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1366x768";
+        // Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+    }
 
     @Test
     void fillFormTest() {
-        //    https://demoqa.com/text-box
+            open("/text-box");
+            $("[id =userName]").setValue("Alex Egorov");
+//        $("#userName").setValue("Alex Egorov"); // аналогичный вариант
+            // два скоращений id = # и class = . (точка)
+
+            $("#userEmail").setValue("a.egorov@mail.ru");
+            $("[id = currentAddress]").setValue("Some adress 1");
+            $("[id =permanentAddress]").setValue("Another adress 1");
+
+            $("[id=submit]").click();
+
+            $("#output").shouldHave(
+                    text("Alex Egorov"),
+                    text("a.egorov@mail.ru"),
+                    text("Some adress 1"),
+                    text("Another adress 1"));
     }
 }
